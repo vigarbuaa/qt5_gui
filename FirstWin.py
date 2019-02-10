fb03199ee81e1942c233fc7623765acec6c252a7
曲方伟 <qufw@htdadao.net>
2019-02-10 15:47:19 +0800

    add font select dialog

--- FirstWin.py
+++ FirstWin.py
@@ -1,9 +1,67 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMenu,QMainWindow, qApp, QAction
+from PyQt5.QtWidgets import QFontDialog,QLabel,QPushButton,QVBoxLayout, QApplication,QWidget,QMenu,QMainWindow, qApp, QAction
 from PyQt5.QtGui import QIcon
 
+# 1. [done] add menu bar ,add several actions
+# 2. add openfile dialog (qt04_QFileDialog.py)
+# 3. add table monitor, open excel/csv file--> show data in table view --> export file
+# 4. plot in table view(pyecharts)
+# 5. plot in table view(pyecharts)
+
+class FontDialogDemo(QWidget):
+    def __init__(self, parent=None):
+        super(FontDialogDemo, self).__init__(parent)
+        layout = QVBoxLayout()
+        self.fontButton  = QPushButton("choose font")
+        self.fontButton .clicked.connect(self.getFont)
+        layout.addWidget(self.fontButton )
+        self.fontLineEdit  = QLabel("Hello,测试字体例子")
+        layout.addWidget(self.fontLineEdit )
+        self.setLayout(layout)
+        self.setWindowTitle("Font Dialog 例子")
+        
+    def getFont(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            self.fontLineEdit.setFont(font)
+            
+########################################################################
+class lineEditDemo(QWidget):
+    """"""
+
+    #----------------------------------------------------------------------
+    def __init__(self,parent=None):
+        """Constructor"""
+        super(lineEditDemo,self).__init__(parent)
+    
+    def initUI(self):
+        self.setwindowtitle("QLineEdit example")
+        flo=QFormLayout()
+        pNormalLineEdit = QLineEdit()
+        pNoEchoLineEdit = QLineEdit()
+        pPasswordLineEdit = QLineEdit( )
+        pPasswordEchoOnEditLineEdit = QLineEdit()
+
+        flo.addRow("Normal", pNormalLineEdit)
+        flo.addRow("NoEcho", pNoEchoLineEdit)
+        flo.addRow("Password", pPasswordLineEdit)
+        flo.addRow("PasswordEchoOnEdit", pPasswordEchoOnEditLineEdit)
+        
+        pNormalLineEdit.setPlaceholderText("Normal")
+        pNoEchoLineEdit.setPlaceholderText("NoEcho")
+        pPasswordLineEdit.setPlaceholderText("Password")
+        pPasswordEchoOnEditLineEdit.setPlaceholderText("PasswordEchoOnEdit")
+
+    # 设置显示效果
+        pNormalLineEdit.setEchoMode(QLineEdit.Normal)
+        pNoEchoLineEdit.setEchoMode(QLineEdit.NoEcho)
+        pPasswordLineEdit.setEchoMode(QLineEdit.Password)
+        pPasswordEchoOnEditLineEdit.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+                            
+        self.setLayout(flo)
+   
 ########################################################################
 class Example(QMainWindow):
     """"""
@@ -34,6 +92,7 @@ class Example(QMainWindow):
     #----------------------------------------------------------------------
     def initUI(self):
         """"""
+        
         self.statusbar = self.statusBar()
         self.statusbar.showMessage("Ready")
         
@@ -63,8 +122,25 @@ class Example(QMainWindow):
         fileMenu.addAction(viewStatAct)
         fileMenu.addAction(exitAct)
         
+        dialogMenu=menubar.addMenu("Dialog")
+        dialogMenu.addAction("file")
+        dialogMenu.addAction("color")
+        
+        self.fontSelectDemo=FontDialogDemo()
+        fontAct=QAction(QIcon("exit.png"),'&Font',self)
+        fontAct.setShortcut('Ctrl+D')
+        fontAct.setStatusTip("font dialog demo")
+        dialogMenu.addAction(fontAct)
+        fontAct.triggered.connect(self.fontSelectDemo.show)
+
+        toolMenu=menubar.addMenu("Tool")
+        toolMenu.addAction('spider')
+        toolMenu.addAction('stock')
+        toolMenu.addAction('qihuo')
+        toolMenu.addAction('futu')
+
         self.setGeometry(300,300,800,150)
-        self.setWindowTitle('simple file menu')
+        self.setWindowTitle('vigar QT 框架')
         self.show()
         
     #---------------pop up menu definition-------------------------------------------------------
